0a229e4712fa481f5bfd756f1854774c8b6b8e49
SarahHale <id15she@cs.umu.se>
2017-10-14 23:55:14 +0200

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top-100
+    }, 500);
+});
+
+$(window).scroll(function () {
+if ($(window).scrollTop() >= 50) {
+$('.menu').css({'background':'rgba(255,255,255,0.9)', 'padding':'5'});
+} else {
+$('.menu').css('background','transparent');
+}
+});
